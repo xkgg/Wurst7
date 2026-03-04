3a8aeecdcd2cc99e89aa4c8a346424deef82f844
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2026-03-04 21:19:01 +0800

    Update FilterInvisibleSetting.java

--- src/main/java/net/wurstclient/settings/filters/FilterInvisibleSetting.java
+++ src/main/java/net/wurstclient/settings/filters/FilterInvisibleSetting.java
@@ -13,7 +13,7 @@ public final class FilterInvisibleSetting extends EntityFilterCheckbox
 {
 	public FilterInvisibleSetting(String description, boolean checked)
 	{
-		super("Filter invisible", description, checked);
+		super("过滤透明生物", description, checked);
 	}
 	
 	@Override
@@ -36,3 +36,4 @@ public final class FilterInvisibleSetting extends EntityFilterCheckbox
 			checked);
 	}
 }
+
