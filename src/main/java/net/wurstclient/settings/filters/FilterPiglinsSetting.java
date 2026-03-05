eaba6487b53c9bbd14ce37bd21b34db296a6dcd3
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2026-03-05 18:46:29 +0800

    Update FilterPiglinsSetting.java

--- src/main/java/net/wurstclient/settings/filters/FilterPiglinsSetting.java
+++ src/main/java/net/wurstclient/settings/filters/FilterPiglinsSetting.java
@@ -18,7 +18,7 @@ public final class FilterPiglinsSetting extends AttackDetectingEntityFilter
 	private FilterPiglinsSetting(String description, Mode selected,
 		boolean checked)
 	{
-		super("Filter piglins", description + EXCEPTIONS_TEXT, selected,
+		super("过滤猪灵", description + EXCEPTIONS_TEXT, selected,
 			checked);
 	}
 	
@@ -41,13 +41,13 @@ public final class FilterPiglinsSetting extends AttackDetectingEntityFilter
 	
 	public static FilterPiglinsSetting genericCombat(Mode selected)
 	{
-		return new FilterPiglinsSetting("When set to \u00a7lOn\u00a7r,"
-			+ " piglins won't be attacked at all.\n\n"
-			+ "When set to \u00a7lIf calm\u00a7r, piglins won't be attacked"
-			+ " until they attack first. Be warned that this filter cannot"
-			+ " detect if the piglins are attacking you or someone else.\n\n"
-			+ "When set to \u00a7lOff\u00a7r, this filter does nothing and"
-			+ " piglins can be attacked.", selected);
+		return new FilterPiglinsSetting("当设置为 \u00a7lOn\u00a7r,"
+			+ " 猪灵根本不会受到攻击。\n\n"
+			+ "当设置为 \u00a7lIf calm\u00a7r, 猪灵不会被攻击"
+			+ " 直到他们先发起攻击。请注意，这个过滤器无法"
+			+ " 检测猪灵是否正在攻击你或其他人。\n\n"
+			+ "当设置为\u00a7lOff\u00a7r, 这个过滤器没有任何作用"
+			+ " 猪灵可以被攻击。", selected);
 	}
 	
 	public static FilterPiglinsSetting genericVision(Mode selected)
@@ -66,3 +66,4 @@ public final class FilterPiglinsSetting extends AttackDetectingEntityFilter
 		return new FilterPiglinsSetting(description, null, onByDefault);
 	}
 }
+
