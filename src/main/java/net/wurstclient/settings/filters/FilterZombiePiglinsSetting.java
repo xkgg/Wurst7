cbc2e142c31d8b1ffab20717e2f9d78afe264285
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2026-03-05 19:00:42 +0800

    Update FilterZombiePiglinsSetting.java

--- src/main/java/net/wurstclient/settings/filters/FilterZombiePiglinsSetting.java
+++ src/main/java/net/wurstclient/settings/filters/FilterZombiePiglinsSetting.java
@@ -16,7 +16,7 @@ public final class FilterZombiePiglinsSetting
 	private FilterZombiePiglinsSetting(String description, Mode selected,
 		boolean checked)
 	{
-		super("Filter zombie piglins", description, selected, checked);
+		super("过滤僵尸猪灵", description, selected, checked);
 	}
 	
 	public FilterZombiePiglinsSetting(String description, Mode selected)
@@ -56,3 +56,4 @@ public final class FilterZombiePiglinsSetting
 		return new FilterZombiePiglinsSetting(description, null, onByDefault);
 	}
 }
+
